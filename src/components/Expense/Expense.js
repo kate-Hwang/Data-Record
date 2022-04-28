f14553c0720086ed0e7b1adb340e4af7a0c929fc
kateHwang <ghkd6385@gmail.com>
2022-04-28 22:26:00 +0900

    commit to 양방향바인딩

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,11 +1,24 @@
 import ExpenseItem from "../Expense/Expenseltem";
+import ExpenseFilter from "../ExpenseFilter";
+
 import '../../assets/css/Expense/Expense.css';
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const Expense = (props) => {
+
+    const [ filteredYear, setFilteredYear ] = useState('2020'); //화면에 기본셀렉트 설정값지정하기
+
+    const filterEventHandler = (selectedYear) => {
+        // console.log('Expense.js');
+        // console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    };
+
     return(
-        <>
+        <>   
             <Card className="expenses">
+                <ExpenseFilter selected={filteredYear} onChangeAddFilter={filterEventHandler}/>
                 <ExpenseItem 
                     title={props.items[0].title} 
                     amount={props.items[0].amount} 
